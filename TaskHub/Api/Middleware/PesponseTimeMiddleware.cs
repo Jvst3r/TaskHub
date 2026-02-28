d725267a700ff082ea932a97c553547df6b2c1e5
Арсений <arsunik07@mail.ru>
2026-02-28 15:28:31 +0500

    Создал второй middleware

---
+++ TaskHub/Api/Middleware/PesponseTimeMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+
+namespace Api.Middleware
+{
+    /// <summary>
+    /// Замеряет время обработки запроса. Также добавляет в ответ заголовок X-Response-Time-Ms. 
+    /// </summary>
+    public class PesponseTimeMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public PesponseTimeMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        /// <summary>
+        /// Засекает время выполнения middleware и добавляет в Responce-заголовки это самое время
+        /// </summary>
+        /// <param name="context">HTTP-контекст</param>
+        /// <returns></returns>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //засекаем время
+            var watch = Stopwatch.StartNew();
+            //ждём выполнение следующего этапа конвейера
+            await next(context);
+
+            watch.Stop();
+            //добавление в заголовок кол-ва миллисекунд
+            context.Response.Headers["X-Response-Time-Ms"] = stopwatch.ElapsedMilliseconds.ToString();
+        }
+
+    }
+}
