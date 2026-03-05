30fce36269473ca65422fdd21cede61831a64325
Арсений <arsunik07@mail.ru>
2026-03-05 14:02:47 +0500

    Создан DisposedService

---
+++ TaskHub/Api/Services/DisposedService.cs
@@ -0,0 +1,25 @@
+﻿namespace Api.Services
+{
+    public class DisposedService : IDisposable
+    {
+        public Guid InstanceId { get; private set; } = Guid.NewGuid();
+        public string ServiceName { get; private set; }
+        public DisposedService(string _serviceName) 
+        { 
+            ServiceName = _serviceName ?? throw new ArgumentNullException("Service without name");
+            //Вывод в консоль о создании
+            Console.WriteLine(
+                $"Created new service!" +
+                $"name : {ServiceName}" +
+                $"Id : {InstanceId}");
+        }
+        public void Dispose()
+        {
+            //вывод в консоль об окончании жизни инстанса
+            Console.WriteLine(
+                "Service was disposed!" +
+                $"name : {ServiceName}" +
+                $"Id : {InstanceId}");
+        }
+    }
+}
