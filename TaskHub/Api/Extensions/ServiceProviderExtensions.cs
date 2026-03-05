7d35207f1862b588f7bdfa28086c6ee17ec3e9f8
Арсений <arsunik07@mail.ru>
2026-03-05 16:54:08 +0500

    extension создан

---
+++ TaskHub/Api/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Api.Services;
+namespace Api.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void ResolveAndCompare<TService>(this IServiceProvider provider) where TService : class, IHasInstanceId
+        {
+            var first = provider.GetRequiredService<TService>();
+            var second = provider.GetRequiredService<TService>();
+
+            Console.WriteLine("Type: " + typeof(TService).Name);
+
+            Console.WriteLine("First: " + first.InstanceId);
+            Console.WriteLine("Second: " + second.InstanceId);
+
+            bool isServicesSame = ReferenceEquals(first, second);
+
+            if (isServicesSame) Console.WriteLine("Services are same!");
+            else Console.WriteLine("Different services!");
+
+        }
+
+    }
+}
