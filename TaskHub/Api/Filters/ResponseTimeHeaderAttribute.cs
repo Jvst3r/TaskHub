ed7b7c6e2dd0f94c57406f8d6cf62e67ffd43945
Арсений <arsunik07@mail.ru>
2026-03-14 18:04:27 +0500

    фильтр с временем запроса

--- TaskHub/Api/Filters/ResponseTimeHeaderAttribute.cs
+++ TaskHub/Api/Filters/ResponseTimeHeaderAttribute.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Api.Filters
 {
@@ -7,13 +10,26 @@ namespace Api.Filters
     {
         public ResponseTimeHeaderAttribute() { }
 
-        public async InvokeAsync(HttpContext context)
+        public override void OnActionExecuting(ActionExecutingContext context)
         {
+            //запускаем часы
             var watch = new Stopwatch();
             watch.Start();
+            //сначала думал писать callback в OnActionExecuted, потом понял что работает замыкание, если написать здесь:)
+            //context.HttpContext.Items["Watch"] = watch;
 
-            context.Response.OnStarting(() =>
-            )
+            //callback
+            context.HttpContext.Response.OnStarting(() =>
+            {
+               //Stopwatch watch = (Stopwatch)context.HttpContext.Items["Watch"];
+                watch.Stop();
+
+                context.HttpContext.Response.Headers.Append("X-Response-Time-Ms", watch.ElapsedMilliseconds.ToString());
+                return Task.CompletedTask;
+            });
         }
+        //можно не переопределять, но я уже написал  ¯\_(ツ)_/¯
+        public override void OnActionExecuted(ActionExecutedContext context) => base.OnActionExecuted(context);
+        
     }
 }
