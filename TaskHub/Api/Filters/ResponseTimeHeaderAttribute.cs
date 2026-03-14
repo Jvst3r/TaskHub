b643f3f6aa1c92bcbc6440e5882218731bdafabb
Арсений <arsunik07@mail.ru>
2026-03-14 15:01:44 +0500

    Создал заготовки

---
+++ TaskHub/Api/Filters/ResponseTimeHeaderAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace Api.Filters
+{
+    public class ResponseTimeHeaderAttribute : ActionFilterAttribute
+    {
+        public ResponseTimeHeaderAttribute() { }
+
+        public async InvokeAsync(HttpContext context)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+
+            context.Response.OnStarting(() =>
+            )
+        }
+    }
+}
