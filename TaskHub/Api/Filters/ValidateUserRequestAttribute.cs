34c9ff4b51dd8a91a2c3b70cf07dba422fce746a
Арсений <arsunik07@mail.ru>
2026-03-14 18:04:58 +0500

    Фильтр с валидацией реквеста

--- TaskHub/Api/Filters/ValidateUserRequestAttribute.cs
+++ TaskHub/Api/Filters/ValidateUserRequestAttribute.cs
@@ -1,6 +1,31 @@
-﻿namespace Api.Filters
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+
+namespace Api.Filters
 {
-    public class ValidateUserRequestAttribute
+    public class ValidateUserRequestAttribute : ActionFilterAttribute
     {
+        public ValidateUserRequestAttribute() { }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            //Обработка пустого запроса
+            var request = context.ActionArguments.Values.FirstOrDefault();
+            if (request == null)
+            {
+                context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
+                return;
+            }
+            
+            //обработка имени через рефлексию
+            var property = request.GetType().GetProperty("Name");
+            var name = property?.GetValue(request) as string;
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                context.Result = new BadRequestObjectResult("Имя пользователя не задано");
+            }
+        }
     }
 }
