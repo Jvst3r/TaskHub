b8b62a08ef5fd7711766bf7208b5686f5fd23e76
Арсений <arsunik07@mail.ru>
2026-03-21 17:18:46 +0500

    Создал IManageTaskUseCase

---
+++ TaskHub/Api/UseCases/Tasks/ManageTaskUseCase.cs
@@ -0,0 +1,43 @@
+﻿using Api.Controllers.Tasks.Response;
+using Api.UseCases.Tasks.Interfaces;
+
+namespace Api.UseCases.Tasks
+{
+    public class ManageTaskUseCase : IManageTaskUseCase
+    {
+        public ManageTaskUseCase() 
+        {
+
+        }
+
+        public Task<TaskResponse> CreateTaskAsync(string title, Guid createdByUserId, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAllTasksAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTaskAsync(Guid id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TaskResponse>> GetAllTasksAsync(CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskResponse?> GetTaskByIdAsync(Guid id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetTaskTitleAsync(Guid id, string title, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
