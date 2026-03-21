46214bd1f15084ea84011c272790ba7c8ee800c8
Арсений <arsunik07@mail.ru>
2026-03-21 14:26:24 +0500

    Я дурак и не сделал с самого начала отдельно контексты БД для юзеров и задач и засунул всё в один файл. Исправил

---
+++ TaskHub/Dal/Context/TaskDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dal.Context
+{
+    public sealed class TaskDbContext : DbContext
+    {
+
+        public TaskDbContext(DbContextOptions<UserDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaskItem>(entity =>
+            {
+                entity.ToTable("tasks");
+
+                entity.HasKey(x=>x.Id);
+
+                entity.Property(x => x.Title)
+                    .HasColumnName("title")
+                    .HasMaxLength(200);
+
+                entity.Property(x => x.CreatedUtc)
+                    .HasColumnName("Created_At_Utc")
+                    .IsRequired();
+
+                entity.HasOne<User>()
+                    .WithMany()
+                    .HasForeignKey(x => x.CreatedByUserId);
+
+                entity.Property(x => x.CreatedByUserId)
+                    .HasColumnName("Created_By_User_Id")
+                    .IsRequired();
+            });
+
+            }
+    }
+}
--- TaskHub/Dal/Context/UserDbContext.cs
+++ TaskHub/Dal/Context/UserDbContext.cs
@@ -17,7 +17,6 @@ public sealed class UserDbContext : DbContext
     /// Пользователи и Задачи
     /// </summary>
     public DbSet<User> Users => Set<User>();
-    public DbSet<TaskItem> Tasks => Set<TaskItem>();
 
     /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -36,29 +35,6 @@ public sealed class UserDbContext : DbContext
                 .HasColumnName("last_activity_utc")
                 .IsRequired();
         });
-        modelBuilder.Entity<TaskItem>(entity =>
-        {
-            entity.ToTable("tasks");
-
-            entity.HasKey(x=>x.Id);
-
-            entity.Property(x => x.Title)
-            .HasColumnName("title")
-            .HasMaxLength(200);
-
-            entity.Property(x => x.CreatedUtc)
-            .HasColumnName("Created_At_Utc")
-            .IsRequired();
-
-            entity.HasOne<User>()
-            .WithMany()
-            .HasForeignKey(x => x.CreatedByUserId);
-
-            entity.Property(x => x.CreatedByUserId)
-            .HasColumnName("Created_By_User_Id")
-            .IsRequired();
-        });
-
         base.OnModelCreating(modelBuilder);
     }
 }
