3e4f03ec8cbc6689f5f038d6f6800d56d77eab7d
Арсений <arsunik07@mail.ru>
2026-03-25 18:54:06 +0500

    сделал фильтр с информацией о студенте(мне)

--- TaskHub/Api/Attributes/Filters/StudentInfoHeadersFilter.cs
+++ TaskHub/Api/Attributes/Filters/StudentInfoHeadersFilter.cs
@@ -4,5 +4,14 @@ namespace Api.Attributes.Filters
 {
     public class StudentInfoHeadersFilter : ActionFilterAttribute
     {
+        private readonly static string StudentName = "Pichugov Arseny Sergeich";
+        private readonly static string StudentGroup = "ri-240911";
+
+        //использую ResultExecuting потому что добавляем заголовки после отработки эндпоинта
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.HttpContext.Response.Headers["X-Studenr-Group"] = StudentGroup;
+            context.HttpContext.Response.Headers["X-Student-Name"] = StudentName;
+        }
     }
 }
