0436a581ebe7b9164b0404da6cda125e8c5163be
Арсений <arsunik07@mail.ru>
2026-03-25 21:17:53 +0500

    написал фильтр-валидатор для создания задачи

--- TaskHub/Api/Attributes/Filters/ValidateCreateTaskRequestFilter.cs
+++ TaskHub/Api/Attributes/Filters/ValidateCreateTaskRequestFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Api.Controllers.Tasks.Request;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -6,17 +7,31 @@ namespace Api.Attributes.Filters
 {
     public class ValidateCreateTaskRequestFilter : ActionFilterAttribute
     {
+        //использую ActionExecuting для валидации до начала выполнения контроллера
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            var body = context.HttpContext.Request.Body;
 
-            if (body == null )
+            var body = context.HttpContext.Request.Body;
+            if (body == null)
             {
                 context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
                 return;
             }
 
-            var id = body.
+            var id = (Guid)context.ActionArguments["id"];
+            if (id == Guid.Empty || 
+                string.IsNullOrEmpty(id.ToString()))
+            {
+                context.Result = new BadRequestObjectResult("Идентификатор пользователя не задан");
+                return;
+            }
+
+            var title = (string)context.ActionArguments["title"];
+            if (string.IsNullOrEmpty(title))
+            {
+                context.Result = new BadRequestObjectResult("Название задачи не задано");
+                return;
+            }
         }
     }
 }
