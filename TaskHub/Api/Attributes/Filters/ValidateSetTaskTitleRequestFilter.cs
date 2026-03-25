e75d3c78c07fee5c20a34ca4a7b7b50e9f08a9ca
Арсений <arsunik07@mail.ru>
2026-03-25 21:31:50 +0500

    написал фильтр-валидатор для изменения названия задачи

--- TaskHub/Api/Attributes/Filters/ValidateSetTaskTitleRequestFilter.cs
+++ TaskHub/Api/Attributes/Filters/ValidateSetTaskTitleRequestFilter.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Api.Attributes.Filters
 {
     public class ValidateSetTaskTitleRequestFilter : ActionFilterAttribute
     {
+        //использую ActionExecuting для валидации до начала выполнения контроллера
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var body = context.HttpContext.Request.Body;
+            if (body == null)
+            {
+                context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
+                return;
+            }
+        }
     }
 }
