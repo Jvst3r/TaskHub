0d6684641c5193720c0b0c2debbfc58c5ee8950f
Арсений <arsunik07@mail.ru>
2026-03-25 19:08:03 +0500

    писал валидатор и начал тупить откуда взять айдишник, ща буду думать

--- TaskHub/Api/Attributes/Filters/ValidateCreateTaskRequestFilter.cs
+++ TaskHub/Api/Attributes/Filters/ValidateCreateTaskRequestFilter.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Api.Attributes.Filters
 {
     public class ValidateCreateTaskRequestFilter : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var body = context.HttpContext.Request.Body;
+
+            if (body == null )
+            {
+                context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
+                return;
+            }
+
+            var id = body.
+        }
     }
 }
