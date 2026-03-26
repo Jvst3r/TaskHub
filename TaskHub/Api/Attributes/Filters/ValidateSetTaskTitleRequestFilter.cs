4e9eb4b7ffa54366bc228c1020ce105c1e0a9713
Арсений <arsunik07@mail.ru>
2026-03-26 14:36:09 +0500

    понял как забрать тело запроса из ActionArguments, но если там null выпадает исключение

--- TaskHub/Api/Attributes/Filters/ValidateSetTaskTitleRequestFilter.cs
+++ TaskHub/Api/Attributes/Filters/ValidateSetTaskTitleRequestFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Api.Controllers.Tasks.Request;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Api.Attributes.Filters
@@ -8,8 +9,12 @@ namespace Api.Attributes.Filters
         //использую ActionExecuting для валидации до начала выполнения контроллера
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            var body = context.HttpContext.Request.Body;
-            if (body == null)
+            //посмотрел что приходит, оказывается всё время это было не body, а request
+            var request = context.ActionArguments["request"];
+
+            var dto = (SetTaskTitleRequest)request;
+
+                if (dto == null || string.IsNullOrEmpty(dto.Title))
             {
                 context.Result = new BadRequestObjectResult("Тело запроса отсутствует");
                 return;
