05a2b59e8375ddd368cc227355bd8161eff5169c
Арсений <arsunik07@mail.ru>
2026-05-09 16:48:44 +0500

    создал свою WebApplicationFactory, удалил для тестов из сервисов настоящую БД и впихнул новую липовую InMemory

---
+++ TaskHub/Tests/IntegrationTests/IntegrationWebApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using Api;
+using Dal.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests.IntegrationTests
+{
+    public class IntegrationWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TaskDbContext>));
+                
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                services.AddDbContext<TaskDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("ForTasksTestDb");
+                });
+            });
+        }
+    }
+}
