47c855feb04f4ceec2bd635f45cfdf999e9c4d98
Арсений <arsunik07@mail.ru>
2026-05-09 17:24:39 +0500

    Сделал первый тест но он не проходит

---
+++ TaskHub/Tests/IntegrationTests/TaskControllerIntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Tests.IntegrationTests
+{
+    public class TaskControllerIntegrationTests : IClassFixture<IntegrationWebApplicationFactory>
+    {
+        private readonly HttpClient client;
+
+        public TaskControllerIntegrationTests(IntegrationWebApplicationFactory factory)
+        {
+            client = factory.CreateClient();
+        }
+
+
+        [Fact]
+        public async Task GetAllTasksWhenSuccess()
+        {
+            var response = await client.GetAsync("/tasks");
+
+            Assert.NotNull(response);
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
